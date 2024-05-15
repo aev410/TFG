430a8522506f93f33f67e240386d653da5d297c7
aev410 <antonio.escamilla@viewnext.com>
2024-05-15 16:57:17 +0200

    instalacion nodemon

--- ProyectoServidor/src/server.js
+++ ProyectoServidor/src/server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const morgan = require('morgan')
-// const { connectDB } = require('./config/database')
+const { connectDB } = require('./config/database')
 require('dotenv').config()
 
 const app = express()
@@ -13,16 +13,16 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-// app.get('/users', async (req, res) => {
-//   connectDB()
-//   try {
-//     const { pool } = require('./config/database')
-//     const result = await pool.query('SELECT * FROM users')
-//     res.json(result.rows)
-//   } catch (error) {
-//     console.error('Error executing query', error.stack)
-//     res.status(500).send('Internal Server Error')
-//   }
-// })
+app.get('/pelis', async (req, res) => {
+  connectDB()
+  try {
+    const { pool } = require('./config/database')
+    const result = await pool.query('SELECT * FROM alpujarraalmeria.usuarios')
+    res.json(result.rows)
+  } catch (error) {
+    console.error('Error executing query', error.stack)
+    res.status(500).send('Internal Server Error')
+  }
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
