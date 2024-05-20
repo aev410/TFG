2e49a0b622fc34534930a82935f8a4be152c8ada
aev410 <antonio.escamilla@viewnext.com>
2024-05-20 09:49:13 +0200

    volumen comun

--- init/init.sql
+++ init/init.sql
@@ -1,11 +1,6 @@
--- Active: 1716075812714@@127.0.0.1@5432@postgres
+-- Active: 1715772260983@@localhost@5432@alpujarra_almeria
 
-drop DATABASE alpujarra_almeria WITH (FORCE);
--- Active: 1715957714479@@127.0.0.1@5432@alpujarra_almeria
 
-create database alpujarra_almeria;
-
-use alpujarra_almeria;
 
 CREATE SCHEMA clientes;
 -- Cambia al esquema "mi_esquema"
