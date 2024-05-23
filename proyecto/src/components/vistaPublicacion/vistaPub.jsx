61e0a3dc0b6fe2db9e4288b89ab8998326bffb56
aev410 <antonio.escamilla@viewnext.com>
2024-05-23 15:39:38 +0200

    creado fichero cliente para manejar peticiones

--- proyecto/src/components/vistaPublicacion/vistaPub.jsx
+++ proyecto/src/components/vistaPublicacion/vistaPub.jsx
@@ -1,31 +1,29 @@
-import axios from 'axios';
-import { useState, useEffect } from 'react';
+import usePublicacion from "../../services/api";
 import "./vistaPub.css"
 
 const VistaPub = ({ id }) => {
-  const [datos, setDatos] = useState(null);
+  const { datos, cargando, error } = usePublicacion(id)
 
-  useEffect(() => {
-    axios.get('http://localhost:3000/publicacion/' + id)
-      .then(function (response) {
-        console.log(response.data);
-        setDatos(response.data);
-      })
-      .catch(function (error) {
-        console.error("Error al buscar publicacion: " + error);
-      });
-  }, [id]);
+  if (cargando) {
+    return <p>Cargando...</p>;
+  }
+
+  if (error) {
+    return <p>{error}</p>;
+  }
+
+  if (!datos) {
+    return null;
+  }
 
   return (
     <div className='tarjeta'>
-    {datos ? (
-        <div>
-          <img src={datos[0].imagenes} alt="Imagen de la publicación" />
-          <h3>{datos[0].nombre}</h3>
-          <p>Precio: {datos[0].precio}</p>
-          <p>Fecha de publicación: {new Date(datos[0].fecha_pub).toLocaleDateString()}</p>
-        </div>
-    ) : 'Cargando...'}
+      <div>
+        <img src={datos[0].imagenes} alt="Imagen de la publicación" />
+        <h3>{datos[0].nombre}</h3>
+        <p>Precio: {datos[0].precio}</p>
+        <p>Fecha de publicación: {new Date(datos[0].fecha_pub).toLocaleDateString()}</p>
+      </div>
     </div>
   );
 }
---
+++ proyecto/src/services/api.js
@@ -0,0 +1,31 @@
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+
+const usePublicacion = (id) => {
+    const [datos, setDatos] = useState(null);
+    const [cargando, setCargando] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.get(`http://localhost:3000/publicacion/${id}`);
+                setDatos(response.data);
+            } catch (error) {
+                console.error("Error al buscar publicación: " + error);
+                setError("Error al buscar publicación");
+            } finally {
+                setCargando(false);
+            }
+        };
+
+        fetchData();
+    }, [id]);
+
+    return { datos, cargando, error };
+};
+
+
+
+
+export default usePublicacion;
