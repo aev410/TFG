413ef26845db8fc7bd88a5766422f0133ff8b4e9
neb0002 <124702061+neb0002@users.noreply.github.com>
2024-06-06 21:59:36 +0200

    navbar, footer terminado

--- proyecto/src/App.js
+++ proyecto/src/App.js
@@ -1,36 +1,42 @@
-import Home from "./pages/home"
+// import Home from "./pages/home"
 import Nav from "./components/nav/nav"
+import Footer2 from "./components/footer2/footer2"
 import Footer from "./components/footer/footer"
-import LoginPage from "./pages/login";
-import Register from "./components/Sessions/register";
-import VistaUser from "./components/vistaUsuario/VistaUser";
-import UploadItem from"./components/nuevPub/UploadItem";
-import PaginaPub from "./components/vistaPublicacion/paginaPub";
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+// import LoginPage from "./pages/login";
+// import Register from "./components/Sessions/register";
+// import VistaUser from "./components/vistaUsuario/VistaUser";
+// import UploadItem from"./components/nuevPub/UploadItem";
+// import PaginaPub from "./components/vistaPublicacion/paginaPub";
+// import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 
 
-import './App.css'
+import './App.css';
 
 function App() {
 
   return (
-    <Router>
-      <Nav />
-      <Link to="/newPub">Go to Publicacion Page</Link>
-      <Link to="/publics/idPub">Go to existing publicacion</Link>
-      <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/login"  element={<><LoginPage/></>}/>
-        <Route path="/register"  element={<><Register/></>}/>
-        <Route path="/user" element={<><VistaUser/></>}/>
-        {/* <Route path="/register/tienda" element={<>  </>}/> */}
-        {/* <Route path="/user/tienda" element={<>  </>}/> */}
-        <Route path="/newPub" element={<><UploadItem/></>}/>
-        {/* <Route path="/publics" element={<>  </>}/> */}
-        <Route path="/publics/idPub" element={<><PaginaPub id={21}/></>}/>
-      </Routes>
-      <Footer />
-    </Router>
+    // <Router>
+    //   <Nav />
+    //   <Link to="/newPub">Go to Publicacion Page</Link>
+    //   <Link to="/publics/idPub">Go to existing publicacion</Link>
+    //   <Routes>
+    //     {/* <Route path="/" element={<Home/>}/> */}
+    //     <Route path="/login"  element={<><LoginPage/></>}/>
+    //     <Route path="/register"  element={<><Register/></>}/>
+    //     <Route path="/user" element={<><VistaUser/></>}/>
+    //     <Route path="/register/tienda" element={<>  </>}/>
+    //     <Route path="/user/tienda" element={<>  </>}/>
+    //     <Route path="/newPub" element={<><UploadItem/></>}/>
+    //     <Route path="/publics" element={<>  </>}/>
+    //     <Route path="/publics/idPub" element={<><PaginaPub id={21}/></>}/>
+    //   </Routes>
+    //   <Footer />
+    // </Router>
+      <div>
+       <Nav />  
+       <Footer2 /> {/*para dispositivos moviles */}
+       <Footer />
+     </div>
 
   );
 }
--- proyecto/src/components/nav/nav.js
+++ proyecto/src/components/nav/nav.js
@@ -1,105 +1,65 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 import "./nav.css";
 import logo from "./img/logo.png";
 
-// Navbar 
+
 function Nav() {
-  return (
-    //   <nav class="navbar navbar-light bg-light p-0">
-    //   <div class="container-fluid">
-    //     <a class="navbar-brand" href=" ">
-    //       <img src={logo} alt="Logo" />
-    //     </a>
-    //     <form class="form-inline d-flex w-50">
-    //       <input class="form-control mr-sm-2 w-100" type="search" placeholder="Buscar" aria-label="Search" />
-    //     </form>
-    //     <div class="d-flex">
-    //       <button class="btn btn-outline-success mr-2" type="button">Regístrate o inicia sesión</button>
-    //       <button class="btn btn-primary" type="button">Vender</button>
-    //     </div>
-    //   </div>
-    // </nav>
-    <nav class="navbar bg-light sticky-top navbar-expand-lg p-0">
-      <div class="container-fluid">
-        <a class="navbar-brand" href=" "><img src={logo} alt="Logo" /></a>
-        <form class="form-inline d-flex w-50">
-           <input class="form-control mr-sm-2 w-100" type="search" placeholder="Buscar" aria-label="Search" />
-        </form>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#main_nav">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="main_nav">
-          <ul class="navbar-nav">
-            <li class="nav-item dropdown has-megamenu">
-              <a class="nav-link dropdown-toggle" href=" " data-bs-toggle="dropdown"><span class="navbar-toggler-icon"></span></a>
-              <div class="dropdown-menu megamenu" role="menu">
-              <div class="row g-3 text-center">		
-						<div class="col-lg-3 col-6">
-							<div class="col-megamenu">
-								<h6 class="title">Title Menu Two</h6>
-								<ul class="list-unstyled">
-									<li><a href=" ">Custom Menu</a></li>
-									<li><a href=" ">Custom Menu</a></li>
-									<li><a href=" ">Custom Menu</a></li>
-									<li><a href=" ">Custom Menu</a></li>
-									<li><a href=" ">Custom Menu</a></li>
-									<li><a href=" ">Custom Menu</a></li>
-								</ul>
-							</div> 
+	return (
+		<div>
+			<nav class="navbar navbar-expand-lg navbar-light bg-light sticky-top p-0 mb-2">
+				<div class="container-fluid">
+					<a class="navbar-brand mx-4" href=" "><img src={logo} alt="Logo" /></a>
+					<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#main_nav">
+						<span class="navbar-toggler-icon"></span>
+					</button>
+					<form class="d-flex w-100">
+						<input class="form-control mx-2 " type="search" placeholder="Buscar" aria-label="Search" />
+					</form>
+					<div class="collapse navbar-collapse" id="main_nav">
+						<button class="btn btn-outline-success mx-5" type="button"><b class=" ">Iniciar Sesion</b></button>
+					</div>
+				</div>
+			</nav>
+			<div class="container-fluid py-2">
+				<div class="row">
+					<div class="col-lg-12">
+						<div class="dropdown">
+							<button class=" btn btn-primary dropdown-toggle my-4 r-5" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
+								Todas las categorias
+							</button>
+							<div class="dropdown-menu w-100" aria-labelledby="dropdownMenuButton">
+								<div class="container">
+									<div class="row text-center">
+										<div class="col-lg-6">
+											<ul class="list-unstyled">
+												<h6 class="dropdown-header">Título 1</h6>
+												<li><hr class="dropdown-divider" /></li>
+												<li><a class="dropdown-item" href=" ">Opción 1</a></li>
+												<li><a class="dropdown-item" href=" ">Opción 2</a></li>
+												<li><a class="dropdown-item" href=" ">Opción 3</a></li>
+												<li><a class="dropdown-item" href=" ">Opción 4</a></li>
+											</ul>
+										</div>
+										<div class="col-lg-6">
+											<ul class="list-unstyled">
+												<h6 class="dropdown-header">Título 1</h6>
+												<li><hr class="dropdown-divider" /></li>
+												<li><a class="dropdown-item" href=" ">Opción 1</a></li>
+												<li><a class="dropdown-item" href=" ">Opción 2</a></li>
+												<li><a class="dropdown-item" href=" ">Opción 3</a></li>
+												<li><a class="dropdown-item" href=" ">Opción 4</a></li>
+											</ul>
+										</div>
+									</div>
+								</div>
+							</div>
 						</div>
-						<div class="col-lg-3 col-6">
-							<div class="col-megamenu">
-								<h6 class="title">Title Menu Two</h6>
-								<ul class="list-unstyled">
-									<li><a href=" ">Custom Menu</a></li>
-									<li><a href=" ">Custom Menu</a></li>
-									<li><a href=" ">Custom Menu</a></li>
-									<li><a href=" ">Custom Menu</a></li>
-									<li><a href=" ">Custom Menu</a></li>
-									<li><a href=" ">Custom Menu</a></li>
-								</ul>
-							</div> 
-						</div>
-						<div class="col-lg-3 col-6">
-							<div class="col-megamenu">
-								<h6 class="title">Title Menu Three</h6>
-								<ul class="list-unstyled">
-									<li><a href=" ">Custom Menu</a></li>
-									<li><a href=" ">Custom Menu</a></li>
-									<li><a href=" ">Custom Menu</a></li>
-									<li><a href=" ">Custom Menu</a></li>
-									<li><a href=" ">Custom Menu</a></li>
-									<li><a href=" ">Custom Menu</a></li>
-								</ul>
-							</div> 
-						</div>    
-						<div class="col-lg-3 col-6">
-							<div class="col-megamenu">
-								<h6 class="title">Title Menu Four</h6>
-								<ul class="list-unstyled">
-									<li><a href=" ">Custom Menu</a></li>
-									<li><a href=" ">Custom Menu</a></li>
-									<li><a href=" ">Custom Menu</a></li>
-									<li><a href=" ">Custom Menu</a></li>
-									<li><a href=" ">Custom Menu</a></li>
-									<li><a href=" ">Custom Menu</a></li>
-								</ul>
-							</div>  
-						</div>
-					</div> 
-              </div>
-            </li>
-          </ul>
-          <ul class="navbar-nav ms-auto">
-            <li class="nav-item">
-			<button class="btn btn-outline-success" type="button">Regístrate o inicia sesión</button>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
-  );
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default Nav;
